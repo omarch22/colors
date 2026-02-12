80da32c2152ff860e67a35eb23000215dd785497
omarch22 <146375360+omarch22@users.noreply.github.com>
2026-02-12 12:44:12 -0600

    Add files via upload

---
+++ Prueba_Plantilla/src/main/java/com/cols/app/PruebaPlantillaApplication.java
@@ -0,0 +1,13 @@
+package com.cols.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPlantillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPlantillaApplication.class, args);
+	}
+
+}
